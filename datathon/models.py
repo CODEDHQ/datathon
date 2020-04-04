a8e52066e59740db13ea9666667217f844375205
lailalelouch <lsb-95@hotmail.com>
2020-04-04 19:14:21 +0300

    add color to levels

--- datathon/models.py
+++ datathon/models.py
@@ -10,6 +10,7 @@ class Dataset(models.Model):
 class Level(models.Model):
 	level = models.IntegerField()
 	points = models.IntegerField()
+	color = models.CharField(max_length=20)
 
 	def __str__(self):
 		return f'level {self.level}'
